0d6fb65d2388f9a077eb84e0edc577978af8c84e
Jacob <jacobmunden@gmail.com>
2022-06-18 16:18:05 -0600

    commit tree a944a968110115b126f486daded640fcfca0675a

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+//test 1
+const main = document.querySelector("#main").remove()
+
+//test 2, 3, 4
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "Jacob is the champion"
+document.appendChild(newHeader)
+console.log(newHeader)
